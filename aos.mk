5dee99c6c9b9e61c2f4cc44c04bbb02498f70c3f
Tissotsaksham <38920222+Tissotsaksham@users.noreply.github.com>
2018-05-07 18:54:12 +0530

    Update and rename unleash.mk to aos.mk

--- aos.mk
+++ aos.mk
@@ -17,7 +17,7 @@
 $(call inherit-product, device/xiaomi/tissot/full_tissot.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/unleash/config/common.mk)
+$(call inherit-product, vendor/aos/config/common.mk)
 
 # A/B updater
 AB_OTA_UPDATER := true
@@ -52,7 +52,7 @@ PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     libgptutils \
     libz
 
-PRODUCT_NAME := unleash_tissot
+PRODUCT_NAME := aos_tissot
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
